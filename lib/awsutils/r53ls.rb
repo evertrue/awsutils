0c2ab631c204d2e956f475f8d8ba60b9849e66a0
Eric Herot <eric.github@herot.com>
2014-05-21 01:14:03 -0400

    Add list and json output formats (and make the tool work)

--- lib/awsutils/r53ls.rb
+++ lib/awsutils/r53ls.rb
@@ -1,53 +1,92 @@
 require 'rubygems'
 require 'fog'
 require 'trollop'
+require 'json'
 
 module AwsUtils
-
   class Route53ListResourceRecord
-
     def connection
       @connection ||= Fog::DNS::AWS.new
     end # def connection
 
-    def parse_opts( args )
-      {:name => args[0]}
+    def parse_opts
+      opts = Trollop.options do
+        opt :format, 'Output format', default: 'table'
+      end
+      opts[:name] = ARGV.last
+      opts
     end # def parse_opts( args )
 
-    def zone_id
-      @zone_id ||= connection.list_hosted_zones.body["HostedZones"].select {|z| z["Name"] == zone_name}.first["Id"]
+    def zone
+      @zone ||= connection.zones.all('domain' => zone_name).first
     end
 
     def zone_name
-      @zone_name ||= @opts[:name].split('.')[-2..-1].join('.') + "."
+      @zone_name ||= @opts[:name].split('.')[-2..-1].join('.') + '.'
     end
 
-    def initialize( args )
-      @opts = parse_opts( args )
+    def apex?
+      @opts[:name].split('.')[-3].nil? ? true : false
+    end
+
+    def initialize
+      @opts = parse_opts
     end # def initialize( args )
 
-    def zone_data
-      @zone_data ||= connection.list_resource_record_sets(zone_id).body
-    end
-
-    def output( record )
-      puts "Name: " + record['Name']
-      puts "Type: " + record['Type']
-      puts "TTL: " + record['TTL']
-      record["ResourceRecords"].each do |rr|
-        puts "  " + rr
+    def display_record(record)
+      if @opts[:format] == 'json'
+        puts JSON.pretty_generate(zone_to_json([record]).first)
+      else
+        puts 'Name: ' + record.name
+        puts 'Type: ' + record.type
+        puts 'TTL: ' + record.ttl
+        puts record.value.count < 2 ? 'Value:' : 'Values:'
+        record.value.each { |rr| puts "  #{rr}" }
       end
     end
 
-    def get_record_by_name
-      name = @opts[:name].split('.').join('.') + "."
-      zone_data.select{|r| r["Name"] == name }.first
+    def record_by_name
+      name = @opts[:name].split('.').join('.') + '.'
+      zone.records.find { |r| r.name == name }
+    end
+
+    def zone_to_json(zone_records)
+      zone_records.map do |r|
+        {
+          'name' => r.name,
+          'type' => r.type,
+          'ttl' => r.ttl,
+          'value' => r.value
+        }
+      end
+    end
+
+    def print_table
+      zone.records.each do |r|
+        printf(
+          "%-40s%-8s%-8s%-40s\n",
+          r.name,
+          r.type,
+          r.ttl,
+          r.value.join(' ')
+        )
+      end
+    end
+
+    def list_all
+      if @opts[:format] == 'json'
+        puts JSON.pretty_generate(zone_to_json(zone.records))
+      else
+        print_table
+      end
     end
 
     def run
-      output( get_record_by_name )
+      if apex?
+        list_all
+      else
+        display_record(record_by_name)
+      end
     end
-
   end # class Route53AddResourceRecord
-
 end # module AwsUtils
