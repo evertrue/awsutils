0be831a7ab2b8cf3106804d9737fba57b9c89108
Eric Herot <eric.github@herot.com>
2015-01-23 11:51:07 -0500

    Minor code beautification

--- lib/awsutils/ec2lsgrp.rb
+++ lib/awsutils/ec2lsgrp.rb
@@ -4,90 +4,86 @@ require 'fog'
 gem 'fog', '>= 1.6.0'
 
 module AwsUtils
+  class Ec2LsGrp
+    def connection
+      @connection ||= Fog::Compute.new(provider: 'AWS')
+    end
 
-	class Ec2LsGrp
+    def lookup
+      if @group =~ /^sg-/
+        group_name = get_group_name(@group)
+      else
+        group_name = @group
+      end
 
-		def connection
-			@connection ||= Fog::Compute.new(:provider => "AWS")
-		end
+      connection.security_groups.get(group_name)
+    end
 
-		def lookup
-			if @group =~ /^sg-/
-				group_name = get_group_name(@group)
-			else
-				group_name = @group
-			end
+    def msg_pair(key, value)
+      puts("#{key} #{value}")
+    end
 
-			return connection.security_groups.get(group_name)
-		end
+    def perms_out(direction, perms)
+      puts "#{direction.upcase} RULES"
+      perms.to_enum.with_index(1) do |perm, index|
+        print "  #{index} "
+        if perm['groups'].count > 0
+          groups_arr = perm['groups'].map do |g|
+            "#{g['groupId']} (#{get_group_name(g['groupId'])})"
+          end
+          print "groups: #{groups_arr.join(', ')}; "
+        end
+        if perm['ipRanges'].count > 0
+          print "ip_ranges: #{perm['ipRanges'].join(', ')}; "
+        end
+        print "ipProtocol: #{perm['ipProtocol']}; "
+        print "fromPort: #{perm['fromPort']}; " if perm['fromPort']
+        print "toPort: #{perm['toPort']}" if perm['toPort']
+        print "\n"
+      end
+    end
 
-		def msg_pair( key, value )
-			puts("#{key} #{value}")
-		end
+    def run
+      g = lookup
 
-		def perms_out( direction, perms )
-			puts "#{direction.upcase} RULES"
-			perms.to_enum.with_index(1) do |perm,index|
+      if g.nil?
+        puts 'No group found by that name.'
+        exit 1
+      end
 
-				print "  #{index} "
-				if perm['groups'].count > 0
-					groups_arr = perm['groups'].map{|g| "#{g['groupId']} (#{get_group_name(g['groupId'])})"}
-					print "groups: #{groups_arr.join(', ')}; "
-				end
-				if perm['ipRanges'].count > 0
-					print "ip_ranges: #{perm['ipRanges'].join(', ')}; "
-				end
-				print "ipProtocol: #{perm['ipProtocol']}; "
-				print "fromPort: #{perm['fromPort']}; " if perm['fromPort']
-				print "toPort: #{perm['toPort']}" if perm['toPort']
-				print "\n"
+      msg_pair('ID', g.group_id)
+      msg_pair('NAME', g.name)
+      msg_pair('OWNER_ID', g.owner_id)
+      msg_pair('DESCRIPTION', g.description)
+      msg_pair('VPC_ID', g.vpc_id) if g.vpc_id
 
-			end
-		end
+      perms_out('incoming', g.ip_permissions)
+      perms_out('egress', g.ip_permissions_egress) if g.vpc_id
+    end
 
-		def run
-			g = lookup
+    def initialize(args)
+      unless args[0]
+        puts 'Please specify a security group'
+        exit 1
+      end
+      @group = args[0]
+    end
 
-			if g.nil?
-				puts "No group found by that name."
-				exit 1
-			end
+    private
 
-			msg_pair('ID', g.group_id)
-			msg_pair('NAME', g.name)
-			msg_pair('OWNER_ID', g.owner_id)
-			msg_pair('DESCRIPTION', g.description)
-			msg_pair('VPC_ID', g.vpc_id) if g.vpc_id
+    def allgroups
+      unless @allgroups
+        @allgroups = {}
+        connection.describe_security_groups.data[:body]['securityGroupInfo']
+          .select { |g| g['groupName'] }.each do |g|
+          @allgroups[g['groupId']] = g['groupName']
+        end
+      end
+      @allgroups
+    end
 
-			perms_out('incoming', g.ip_permissions)
-			perms_out('egress', g.ip_permissions_egress) if g.vpc_id
-		end
-
-		def initialize( args )
-			if ! args[0]
-				puts "Please specify a security group"
-				exit 1
-			end
-			@group = args[0]
-		end
-
-		private
-
-		def allgroups
-			if ! @allgroups
-				@allgroups = {}
-				connection.describe_security_groups().data[:body]["securityGroupInfo"].select{|g| g["groupName"]}.each do |g|
-						@allgroups[g["groupId"]] = g["groupName"]
-				end
-			end
-			return @allgroups
-		end
-
-
-		def get_group_name( id )
-			allgroups[id]
-		end
-
-	end
-
+    def get_group_name(id)
+      allgroups[id]
+    end
+  end
 end
