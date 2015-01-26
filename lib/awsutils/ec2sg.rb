5ae88d7fa95055a3b4fce033bb28242bc053cf21
Eric Herot <eric.github@herot.com>
2015-01-26 13:59:00 -0500

    ec2sg: Add method to find group references in other groups

--- lib/awsutils/ec2sg.rb
+++ lib/awsutils/ec2sg.rb
@@ -9,6 +9,27 @@ module AwsUtils
       @connection ||= Fog::Compute.new(provider: 'AWS')
     end
 
+    def references(search)
+      if search =~ /^sg-/
+        search_id = search
+      else
+        search_id = groups.find { |g| g.name == search }.group_id
+      end
+
+      groups.select do |grp|
+        !grp.ip_permissions.select do |ip_perm|
+          !ip_perm['groups'].select { |src_grp|
+            src_grp['groupName'] == search ||
+              src_grp['groupId'] == search_id
+          }.empty?
+        end.empty?
+      end
+    end
+
+    def groups
+      @groups ||= connection.security_groups
+    end
+
     def assigned?
       servers_using_group = connection.servers.map do |server|
         next unless server.state != 'terminated' &&
