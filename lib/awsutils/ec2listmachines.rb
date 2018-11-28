f14b869762bf958644fa4dff1d2b8c67a47e9a4e
Eric Herot <400447+eherot@users.noreply.github.com>
2018-11-28 14:12:18 -0500

    Linting cleanup of ec2listmachines

--- lib/awsutils/ec2listmachines.rb
+++ lib/awsutils/ec2listmachines.rb
@@ -5,105 +5,114 @@ require 'colorize'
 
 GROUPS_MAX_LENGTH = 96
 
-class Array
-  def longest
-    length = 0
-    val = ''
-    each do |a|
-      len_new = a.length
-      if len_new > length
-        length = len_new
-        val = a
-      end
-    end
-    val
-  end
-end
-
 module AwsUtils
   class Ec2ListMachines
+    def run
+      servers_sorted = formatted_servers.sort_by { |server| server.fetch(opts[:sort].to_sym, '') }
+
+      # Clear all formatting
+      printf "\033[0m"
+
+      items = bold_header + servers_sorted.map do |server|
+        columns.keys.map do |col|
+          # Set an empty string here so that CSV ends up with the right number of cols even when
+          # a field is unset
+          server[col] || ''
+        end
+      end.flatten
+
+      if opts[:csv]
+        hl = HighLine::List.new items, cols: columns.count
+        hl.row_join_string = ','
+        puts hl.to_s
+      else
+        puts HighLine.new.list items, :uneven_columns_across, columns.count
+      end
+    end
+
+    private
+
     def connect
       @connect ||= Aws::EC2::Client.new
     end
 
-    def get_servers
+    def servers
+      return connect.describe_instances.reservations.map(&:instances).flatten unless opts[:search]
+
+      connect.describe_instances.reservations.map(&:instances).flatten.select do |i|
+        (name_tag = i.tags.find { |t| t.key == 'Name' }) &&
+          name_tag.value =~ /.*#{opts[:search]}.*/
+      end
+    end
+
+    def include_terminated?
+      opts[:terminated] || opts[:state] == 'terminated'
+    end
+
+    # rubocop:disable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
+    def include_server?(server)
+      return false if (opts[:state] && server.state.name != opts[:state]) ||
+                      (opts[:type] && server.instance_type != opts[:type]) ||
+                      (opts[:zone] && server.placement.availability_zone != opts[:zone]) ||
+                      (server.state.name == 'terminated' && !include_terminated?)
+
+      true
+    end
+    # rubocop:enable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
+
+    # rubocop:disable Metrics/AbcSize, Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
+    def formatted_servers
       static_ips = connect.describe_addresses.addresses.map(&:public_ip)
 
-      group_map = connect.describe_security_groups.security_groups.each_with_object({}) do |g, m|
-        m[g.group_name] = g.group_id
-      end
-
-      servers =
-        if opts[:search]
-          connect.describe_instances.reservations.map { |r| r.instances }.flatten.select do |i|
-            if (name_tag = i.tags.find { |t| t.key == 'Name' })
-              name_tag.value =~ /.*#{opts[:search]}.*/
-            else
-              false
-            end
-          end
-        else
-          connect.describe_instances.reservations.map { |r| r.instances }.flatten
-        end
-
-      servers.each_with_object([]) do |s, m|
-        next unless (opts[:state].nil? || (s.state.name == opts[:state])) &&
-          (
-            (opts[:terminated] == true) ||
-            (s.state.name != 'terminated') ||
-            (opts[:state] == 'terminated')
-          ) &&
-          (opts[:type].nil? || (s.instance_type == opts[:type])) &&
-          (opts[:zone].nil? || (s.placement.availability_zone == opts[:zone]))
-
-        if s.public_ip_address
-          static_ip = static_ips.include?(s.public_ip_address) ? '(S)' : '(D)'
-        end
-
+      servers.select { |server| include_server? server }.map do |server|
         o = {
-          date: s.launch_time.to_s,
-          az: s.placement.availability_zone,
-          id: s.instance_id,
-          subnet: [s.subnet_id, "(#{subnet_name[s.subnet_id]})"].join(' '),
-          priv_ip: s.private_ip_address,
-          type: s.instance_type,
-          vpc: s.vpc_id,
-          state: opts[:csv] ? s.state.name : colorize_state(s.state.name).bold
+          date: server.launch_time.to_s,
+          az: server.placement.availability_zone,
+          id: server.instance_id,
+          subnet: [server.subnet_id, "(#{subnet_name[server.subnet_id]})"].join(' '),
+          priv_ip: server.private_ip_address,
+          type: server.instance_type,
+          vpc: server.vpc_id,
+          state: opts[:csv] ? server.state.name : colorize_state(server.state.name).bold
         }
 
         if opts[:groups]
-          groups_string = s.security_groups.map { |g| "#{g.group_id} (#{g.group_name})" }.join(', ')
+          groups_string =
+            server.security_groupserver.map { |g| "#{g.group_id} (#{g.group_name})" }.join(', ')
 
           # Shorten the groups string to a manageable length
-          unless opts[:csv] || opts[:all_groups]
-            groups_string = groups_string[0..GROUPS_MAX_LENGTH] + '...' if groups_string.length > GROUPS_MAX_LENGTH
+          unless (opts[:csv] || opts[:all_groups]) && groups_string.length > GROUPS_MAX_LENGTH
+            groups_string = groups_string[0..GROUPS_MAX_LENGTH] + '...'
           end
 
           o[:groups] = groups_string
         end
 
-        o[:vpc] = [s.vpc_id, "(#{vpc_name[s.vpc_id]})"].join(' ') if s.vpc_id && opts[:vpc]
-        o[:pub_ip] = [s.public_ip_address, static_ip].join(' ') if s.public_ip_address
+        if server.vpc_id && opts[:vpc]
+          o[:vpc] = [server.vpc_id, "(#{vpc_name[server.vpc_id]})"].join(' ')
+        end
 
-        serv = s
+        if server.public_ip_address
+          static_ip = static_ips.include?(server.public_ip_address) ? '(S)' : '(D)'
+          o[:pub_ip] = [server.public_ip_address, static_ip].join(' ')
+        end
 
-        opts[:tags].each do |tag|
-          next unless (k = s.tags.find { |t| t.key == tag })
+        # Always include the name tag regardless of cli args (for searching)
+        (opts[:tags] | %w[tag_Name]).each do |tag|
+          next unless (k = server.tags.find { |t| t.key == tag })
+
           o["tag_#{tag}".to_sym] = k.value
         end
 
-        # Always include the name tag anyway for searching
-        if !o[:tag_Name] && (n = s.tags.find { |t| t.key == 'Name' })
-          o[:tag_Name] = n.value
-        end
-
-        m << o
+        o
       end
     end
+    # rubocop:enable Metrics/AbcSize, Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
 
     def vpc_name
       @vpc_name ||= connect.describe_vpcs.vpcs.each_with_object({}) do |v, m|
         next unless (tag = v.tags.find { |t| t.key == 'Name' })
+
         m[v.vpc_id] = tag.value
       end
     end
@@ -111,6 +120,7 @@ module AwsUtils
     def subnet_name
       @subnet_name ||= connect.describe_subnets.subnets.each_with_object({}) do |s, m|
         next unless (tag = s.tags.find { |t| t.key == 'Name' })
+
         m[s.subnet_id] = tag.value
       end
     end
@@ -120,21 +130,21 @@ module AwsUtils
 
       o = { id: 'ID' }
 
-      o.merge!({
+      o.merge!(
         az: 'AZ',
         subnet: 'Subnet',
         priv_ip: 'Private IP'
-      })
+      )
 
       o[:groups] = 'Security Groups' if opts[:groups]
       o[:vpc] = 'VPC' if opts[:vpc]
       o[:created] = 'Created' if opts[:dates]
 
-      o.merge!({
+      o.merge!(
         type: 'Type',
         state: 'State',
         pub_ip: 'Public IP'
-      })
+      )
 
       opts[:tags].each { |t| o["tag_#{t}".to_sym] = "Tag:#{t}" }
 
@@ -154,37 +164,15 @@ module AwsUtils
       end
     end
 
-    def render_list(servers_sorted)
-      # Clear all formatting
-      printf "\033[0m"
-
-      items = []
-
-      # Bold the header line
-      columns.values.each_with_index do |v, i|
-        if i == 0
-          items << "\033[1m#{v}"
+    def bold_header
+      columns.values.each_with_index.map do |v, i|
+        if i.zero?
+          "\033[1m#{v}"
         elsif i == columns.count - 1
-          items << "#{v}\033[0m"
+          "#{v}\033[0m"
         else
-          items << v
+          v
         end
-      end
-
-      items += servers_sorted.map do |server|
-        columns.keys.map do |col|
-          # Set an empty string here so that CSV ends up with the right number of cols even when
-          # a field is unset
-          server[col] || ''
-        end
-      end.flatten
-
-      if opts[:csv]
-        hl = HighLine::List.new items, cols: columns.count
-        hl.row_join_string = ','
-        puts hl.to_s
-      else
-        puts HighLine.new.list items, :uneven_columns_across, columns.count
       end
     end
 
@@ -192,7 +180,7 @@ module AwsUtils
       @opts ||= begin
         opts = Optimist.options do
           opt :sort, 'Sort order', short: 's', default: 'tag_Name'
-          opt :tags, 'Tags to display', short: 'T', default: %w(Name)
+          opt :tags, 'Tags to display', short: 'T', default: %w[Name]
           opt :groups, 'Display Security Groups', default: false
           opt :state, 'State', short: 'S', type: String
           opt :type, 'Type', short: 'F', type: String
@@ -209,10 +197,6 @@ module AwsUtils
 
         opts
       end
-    end
-
-    def run
-      render_list get_servers.sort_by { |server| server.fetch(opts[:sort].to_sym, '') }
     rescue Interrupt
       puts 'Interrupted by user (SIGINT, Ctrl+C, etc.)'
     end
